104acb4cb8cbb4426cd71c1f64879b13d8c382a5
Liu Yue <hangxingliu@gmail.com>
2024-05-05 10:25:31 +0800

    feat: Add a folding range provider for VSCode

---
+++ src/vscode-folding-range.ts
@@ -0,0 +1,28 @@
+import {
+    TextDocument,
+    CancellationToken,
+    FoldingRangeProvider,
+    FoldingContext,
+    FoldingRange,
+    EventEmitter,
+    FoldingRangeKind,
+} from "vscode";
+import { tokenizer } from "./parser-v2/tokenizer";
+import { getFoldingRanges } from "./parser-v2/get-folding-ranges";
+import { SystemdDocumentManager } from "./vscode-documents";
+import { SystemdFileType, systemdFileTypeNames } from "./parser/file-info";
+
+export class SystemdFoldingRange implements FoldingRangeProvider {
+    private _onDidChangeFoldingRanges = new EventEmitter<void>();
+    readonly onDidChangeFoldingRanges = this._onDidChangeFoldingRanges.event;
+
+    provideFoldingRanges(document: TextDocument, context: FoldingContext, token: CancellationToken) {
+        const fileType = SystemdDocumentManager.instance.getType(document);
+        const typeName = systemdFileTypeNames[fileType];
+        console.log(`provideFoldingRanges("${document.uri.toString()}", "${typeName}")`);
+
+        const { tokens } = tokenizer(document.getText(), { mkosi: fileType === SystemdFileType.mkosi });
+        const ranges = getFoldingRanges(tokens);
+        return ranges.map((it) => new FoldingRange(it[0], it[1], it[2] ? FoldingRangeKind[it[2]] : undefined));
+    }
+}
